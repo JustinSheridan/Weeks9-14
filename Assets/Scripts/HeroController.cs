973e7bca8599595b0544317c82fdc685612cf83d
JustinSheridan <macazo@sheridancollege.ca>
2026-07-29 21:07:10 -0400

    CodingGym Week11

--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -5,15 +5,14 @@ public class HeroController : MonoBehaviour
     public Animator heroAnimator;
     public bool isMoving = false;
     
-
-    public AudioSource footstepAudioSource;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioSource footstepAudioSource1;
+    public AudioSource footstepAudioSource2;
+    public AudioSource footstepAudioSource3;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         heroAnimator.SetBool("isMoving", isMoving);
@@ -21,6 +20,23 @@ public class HeroController : MonoBehaviour
 
     public void OnFootstep()
     {
-        footstepAudioSource.Play();
+        int randomIndex = Random.Range(1, 3);
+        if (randomIndex == 1)
+        {
+            footstepAudioSource1.Play();
+        }
+        else if (randomIndex == 2)
+        {
+            footstepAudioSource2.Play();
+        }
+        else if (randomIndex == 3)
+        {
+            footstepAudioSource3.Play();
+        }
+        else
+        {
+            Debug.Log("No sound available to play");
+                
+        }
     }
 }
--- Assets/Scripts/LineMaker.cs
+++ Assets/Scripts/LineMaker.cs
@@ -9,7 +9,12 @@ public class LineMaker : MonoBehaviour
     
     public float growDuration = 1f; 
     public Vector3 startPosition;
+    
+    //Where the mouse is
     public Vector3 endPosition;
+
+    // Where the gameobject is when teleported
+    private Vector3 objectPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,47 +29,72 @@ public class LineMaker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 mousePosition = Mouse.current.position.ReadValue();
+        Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        worldMousePosition.z = 0f; // Lock to the Z-plane (standard for 2D/isometric)
+
+        // Also store this position as our line target
+        endPosition = worldMousePosition;
         
+
+        // Start the line drawing coroutine if not already running
+        if (growCoroutine != null)
+        {
+            StopCoroutine(growCoroutine);
+        }
+        growCoroutine = StartCoroutine(GrowUpdate());
     }
 
-    public void ONJump(InputAction.CallbackContext context)
+    // public void ONJump(InputAction.CallbackContext context)
+    // {
+    //     if (context.performed)
+    //     {
+    //         // If there is already a grow coroutine running, stop it to restart smoothly
+    //         if (growCoroutine != null)
+    //         {
+    //             StopCoroutine(growCoroutine);
+    //         }
+    //
+    //         // Start the new coroutine
+    //         growCoroutine = StartCoroutine(GrowUpdate());
+    //     }
+    // }
+
+    public void ONAttack(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
-            // If there is already a grow coroutine running, stop it to restart smoothly
-            if (growCoroutine != null)
-            {
-                StopCoroutine(growCoroutine);
-            }
-
-            // Start the new coroutine
-            growCoroutine = StartCoroutine(GrowUpdate());
+            transform.position = endPosition;
+            objectPosition = endPosition;
         }
     }
 
     IEnumerator GrowUpdate()
     {
-        float t = 0;
+        // Comment out, the line is always drawn
         
-        // Ensure we have points to render
-        lineRenderer.positionCount = 2;
+        // float t = 0;
+        //
+        // // Ensure we have points to render
+        // lineRenderer.positionCount = 2;
+        //
+        // // Initialize both ends at start position so it starts from "zero" length
+        // lineRenderer.SetPosition(0, startPosition);
+        // lineRenderer.SetPosition(1, startPosition);
+        //
+        // while (t < growDuration)
+        // {
+        //     t += Time.deltaTime;
+        //     
+        //     // Update the second point to interpolate between start and end
+        //     Vector3 currentSecondPosition = Vector3.Lerp(startPosition, endPosition, t / growDuration);
+        //     lineRenderer.SetPosition(1, currentSecondPosition);
+        //
+        //     yield return null;
+        // }
         
-        // Initialize both ends at start position so it starts from "zero" length
-        lineRenderer.SetPosition(0, startPosition);
-        lineRenderer.SetPosition(1, startPosition);
-
-        while (t < growDuration)
-        {
-            t += Time.deltaTime;
-            
-            // Update the second point to interpolate between start and end
-            Vector3 currentSecondPosition = Vector3.Lerp(startPosition, endPosition, t / growDuration);
-            lineRenderer.SetPosition(1, currentSecondPosition);
-
-            yield return null;
-        }
-        
-        // Ensure final position is exact
-        lineRenderer.SetPosition(1, endPosition);
+        // Set position index one and return null
+        lineRenderer.SetPosition(1, endPosition-objectPosition);
+        yield return null;
     }
 }
